23de3fbb8c49c8fcb8343952a5146fc9cb0e35e5
Ulises Cruz <ulises.cruz@tecnoin.com.sv>
2023-06-15 19:19:09 -0600

    clase Empleado base generado

--- Empleado.h
+++ Empleado.h
@@ -1,18 +1,85 @@
 #ifndef EMPLEADO_H
 #define EMPLEADO_H
 
+#include <iostream>
 #include <string>
 
 class Empleado {
+protected:
+    std::string nombre;
+    std::string direccion;
+    std::string fechaNacimiento;
+    std::string sexo;
+
 public:
-    Empleado(const std::string& nombre, int edad);
+    Empleado(const std::string& nombre, const std::string& direccion, const std::string& fechaNacimiento, const std::string& sexo)
+        : nombre(nombre), direccion(direccion), fechaNacimiento(fechaNacimiento), sexo(sexo) {}
 
-    void saludar() const;
-    void esMayorDeEdad() const;
+    virtual ~Empleado() {}
 
+    virtual double calcularSalario() const = 0;
+
+};
+
+class Gerente : public Empleado {
 private:
-    std::string nombre_;
-    int edad_;
+    double sueldo;
+
+public:
+    Gerente(const std::string& nombre, const std::string& direccion, const std::string& fechaNacimiento, const std::string& sexo)
+        : Empleado(nombre, direccion, fechaNacimiento, sexo), sueldo(5000.0) {}
+
+    double calcularSalario() const {
+        // Realizar cálculos para el salario del gerente con descuentos
+        return sueldo - (sueldo * 0.075) - (sueldo * 0.0775);
+    }
+
+};
+
+class Tecnico : public Empleado {
+private:
+    double sueldo;
+
+public:
+    Tecnico(const std::string& nombre, const std::string& direccion, const std::string& fechaNacimiento, const std::string& sexo)
+        : Empleado(nombre, direccion, fechaNacimiento, sexo), sueldo(350.0) {}
+
+    double calcularSalario() const {
+        // Realizar cálculos para el salario del técnico con descuentos
+        return sueldo - (sueldo * 0.075) - (sueldo * 0.0775);
+    }
+
+
+};
+
+class JefeDeArea : public Empleado {
+private:
+    double sueldo;
+
+public:
+    JefeDeArea(const std::string& nombre, const std::string& direccion, const std::string& fechaNacimiento, const std::string& sexo)
+        : Empleado(nombre, direccion, fechaNacimiento, sexo), sueldo(1500.0) {}
+
+    double calcularSalario() const {
+        // Realizar cálculos para el salario del jefe de área con descuentos
+        return sueldo - (sueldo * 0.075) - (sueldo * 0.0775);
+    }
+
+};
+
+class Supervisor : public Empleado {
+private:
+    double sueldo;
+
+public:
+    Supervisor(const std::string& nombre, const std::string& direccion, const std::string& fechaNacimiento, const std::string& sexo)
+        : Empleado(nombre, direccion, fechaNacimiento, sexo), sueldo(750.0) {}
+
+    double calcularSalario() const {
+        // Realizar cálculos para el salario del supervisor con descuentos
+        return sueldo - (sueldo * 0.075) - (sueldo * 0.0775);
+    }
+
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
-#include <iostream>
 #include "Empleado.h"
-
-using namespace std;
+#include <iostream>
 
 int main() {
-    Empleado empleado("Juan", 25);
+    // Crear instancias de diferentes subclases de Empleado
+    Empleado* empleado1 = new Gerente("John Doe", "123 Main St", "01/01/1990", "Masculino");
+    Empleado* empleado2 = new Tecnico("Jane Smith", "456 Elm St", "02/02/1995", "Femenino");
+    Empleado* empleado3 = new JefeDeArea("Mike Johnson", "789 Oak St", "03/03/1985", "Masculino");
+    Empleado* empleado4 = new Supervisor("Amy Davis", "987 Pine St", "04/04/1992", "Femenino");
 
-    empleado.saludar();
-    empleado.esMayorDeEdad();
+    // Calcular y mostrar el salario de cada empleado
+    std::cout << "Salario del gerente: $" << empleado1->calcularSalario() << std::endl;
+    std::cout << "Salario del técnico: $" << empleado2->calcularSalario() << std::endl;
+    std::cout << "Salario del jefe de área: $" << empleado3->calcularSalario() << std::endl;
+    std::cout << "Salario del supervisor: $" << empleado4->calcularSalario() << std::endl;
+
+    // Liberar la memoria asignada a los objetos empleados
+    delete empleado1;
+    delete empleado2;
+    delete empleado3;
+    delete empleado4;
 
     return 0;
 }
